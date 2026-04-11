e5b5890ad5c69a77c88fb799f7c761c94dfcbb27
DESKTOP-DGCLESA\pc1 <dimasrizkyaf@gmail.com>
2026-04-11 14:19:07 +0700

    update json

--- Runtime/GlobalResolver.cs
+++ Runtime/GlobalResolver.cs
@@ -2,6 +2,6 @@ using VContainer;
 
 public static class GlobalResolver
 {
-    // Ini akan menampung Resolver dari ProjectLifetimeScope
+    // Ini akan menampung Resolver dari AudioLifetimeScope
     public static IObjectResolver Instance { get; set; }
 }
